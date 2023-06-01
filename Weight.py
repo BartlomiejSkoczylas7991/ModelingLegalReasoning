c142836bbaf5244acb425d16601d5b31415f4888
Bartlomiej Skoczylas <BartlomiejSkoczylas7991@gmail.com>
2023-06-01 21:38:46 +0200

    Usunięcie istniejących plików

--- Weight.py
+++
@@ -1,7 +0,0 @@
-from scale import Scale
-
-
-class Weight:
-    def __init__(self, scale: Scale, value: int):
-        self.scale = scale
-        self.value = value
--- scale.py
+++
@@ -1,6 +0,0 @@
-from typing import List
-
-
-class Scale:
-    def __init__(self, elements: List[int]):
-        self.elements = elements

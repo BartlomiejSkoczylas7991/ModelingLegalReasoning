290111a26854093792f6c3f81a44e9447a9b7fb2
Bartlomiej Skoczylas <BartlomiejSkoczylas7991@gmail.com>
2023-07-27 01:08:47 +0200

    Controller

---
+++ src/main/test/com/bskoczylas/modelinglegalreasoning/domain/models/facade/logicApp/consortium/ListConsortiumTest.java
@@ -0,0 +1,6 @@
+package com.bskoczylas.modelinglegalreasoning.domain.models.facade.logicApp.consortium;
+
+import static org.junit.jupiter.api.Assertions.*;
+class ListConsortiumTest {
+  
+}
---
+++ src/main/test/com/bskoczylas/modelinglegalreasoning/domain/models/facade/logicApp/reasoningChain/ListReasoningChainTest.java
@@ -0,0 +1,6 @@
+package com.bskoczylas.modelinglegalreasoning.domain.models.facade.logicApp.reasoningChain;
+
+import static org.junit.jupiter.api.Assertions.*;
+class ListReasoningChainTest {
+  
+}

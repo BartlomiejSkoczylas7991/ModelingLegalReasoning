4732d769e37f41b8e805a58e7a394087ac184dca
Bartlomiej Skoczylas <BartlomiejSkoczylas7991@gmail.com>
2023-08-28 19:51:13 +0200

    finish

---
+++ src/main/java/com/bskoczylas/modelinglegalreasoning/App_1.java
@@ -0,0 +1,9 @@
+package com.bskoczylas.modelinglegalreasoning;
+
+import com.bskoczylas.modelinglegalreasoning.App;
+
+public class App_1 {
+    public static void main(String[] args) {
+        App.main(args);
+    }
+}
